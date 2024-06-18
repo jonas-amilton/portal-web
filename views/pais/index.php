51eecf7aec909100173b30ad2de4b06fc714787b
Jonas Silva <jonas96@edu.unisinos.br>
2024-06-18 20:48:58 -0300

    first commit: iniciando a aplicação

---
+++ views/pais/index.php
@@ -0,0 +1,16 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\LinkPager;
+?>
+<h1>Países</h1>
+<ul>
+    <?php foreach ($paises as $pais) : ?>
+    <li>
+        <?= Html::encode("{$pais->nome} ({$pais->codigo})") ?>:
+        <?= $pais->populacao ?>
+    </li>
+    <?php endforeach; ?>
+</ul>
+
+<?= LinkPager::widget(['pagination' => $paginacao]) ?>
