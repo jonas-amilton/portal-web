5232fa5f0c564c0e0bf4d238330df33fbffec371
jonas-amilton <jonas96@edu.unisinos.br>
2024-07-01 20:44:10 -0300

    feat: Adicionado possibilidade de admin alterar status do suporte

--- views/site/painel-administrativo.php
+++ views/site/painel-administrativo.php
@@ -4,7 +4,7 @@
 /** @var yii\bootstrap5\ActiveForm $form */
 /** @var app\models\PainelAdministrativo $model */
 
-use yii\bootstrap5\ActiveForm;
+use app\models\Users;
 use yii\bootstrap5\Html;
 
 $this->title = 'Painel Administrativo';
@@ -13,5 +13,38 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="site-contact">
     <h1><?= Html::encode($this->title) ?></h1>
 
-
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Usuário</th>
+                <th>Mensagem</th>
+                <th>Data de Criação</th>
+                <th>Status</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($messages as $message) : ?>
+            <tr>
+                <td><?= Html::encode($message->id) ?></td>
+                <td><?= Html::encode(Users::getUsernameById($message->user_id)) ?></td>
+                <td><?= Html::encode($message->message) ?></td>
+                <td><?= Html::encode($message->created_at) ?></td>
+                <td><?= Html::encode($message->status) ?></td>
+                <td>
+                    <?= Html::a(
+                            'Alterar Status',
+                            [
+                                'site/update-status',
+                                'id' => $message->id
+                            ],
+                            [
+                                'class' => 'btn btn-primary'
+                            ]
+                        ) ?>
+                </td>
+            </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
 </div>
